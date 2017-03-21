a8c10afe05428e5d0c95b63a86f3d7f7d01bab8c
Vihang Patel <vihangpatel@Vihang-Patel.local>
2017-03-21 17:56:58 +0530

    Export samay from index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var samay = require('./samay').samay;
-
+var samay = require('./samay');
+// Export from here
 module.exports = samay;
-
-
-
-var ob1 = samay('20170105', 'YYYYMMDD' );
-var ob2 = samay('20170606', 'YYYYMMDD');
-var ob3 = samay('20140106', 'YYYYMMDD');
-
-console.log(ob3.isAfter(new Date));
