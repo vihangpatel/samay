6ade7f475a4976d53a82f2284f67890c68cafd92
Vihang Patel <vihangpatel@Vihang-Patel.local>
2017-03-31 12:57:45 +0530

    Add test cases

--- test/spec.js
+++ test/spec.js
@@ -14,4 +14,27 @@ describe('samay.js test suite', function(){
 		expect(samay('20180330', 'YYYYMMDD').format('MM/DD/YYYY HH:mm')).toBe('03/30/2018 00:00')
 	});
 
+	it('format test YYYYMMDD against format MM/DD/YYYY HH:mm', function() {
+		expect(samay('20180330', 'YYYYMMDD').format('MM/DD/YYYY HH:mm')).toBe('03/30/2018 00:00')
+	});
+
+	it('format test YYYYMMDD against format MM/DD/YYYY HH:mm', function() {
+		expect(samay('20180330', 'YYYYMMDD').subtract(2,'hour').format('MM/DD/YYYY HH:mm')).toBe('03/29/2018 22:00')
+	});
+
+	it('format test YYYYMMDD against format MM/DD/YYYY HH:mm', function() {
+		expect(samay('20180101', 'YYYYMMDD').subtract(2,'hour').format('MM/DD/YYYY HH:mm')).toBe('12/31/2017 22:00')
+	});
+
+	it('format test YYYYMMDD against format MM/DD/YYYY HH:mm', function() {
+		expect(samay('20170301', 'YYYYMMDD').subtract(2,'hour').format('MM/DD/YYYY HH:mm')).toBe('02/28/2017 22:00')
+	});
+
+	it('format test YYYYMMDD against format MM/DD/YYYY HH:mm', function() {
+		expect(samay('20170301', 'YYYYMMDD').subtract(2,'days').format('MM/DD/YYYY HH:mm')).toBe('02/27/2017 00:00')
+	});
+
+	it('format test YYYYMMDD against format MM/DD/YYYY HH:mm', function() {
+		expect(samay('201712311545', 'YYYYMMDDHHmm').add(2,'days').subtract(2,'hour').format('MM/DD/YYYY HH:mm')).toBe('01/02/2018 13:45')
+	});
 });
