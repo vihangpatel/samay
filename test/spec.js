dea43dfaf5164f8b35760df3a5825b6f221b67f0
Vihang Patel <vihangpatel@Vihang-Patel.local>
2017-03-31 16:44:19 +0530

    Change const to var

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,6 @@
 describe('samay.js test suite', function(){
 
-	const samay = require('../samay').samay;
+	var samay = require('../samay').samay;
 
 	it('constructor with no arguments', function() {
 		expect(+samay().originalDate === +(new Date)).toBe(true)
