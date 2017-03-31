b2db6b56c3464ad2e2ef5c2dd6f0a232d4f1b2eb
Vihang Patel <vihangpatel@Vihang-Patel.local>
2017-03-31 10:41:48 +0530

    Adding basic test cases

--- test/spec.js
+++ test/spec.js
@@ -6,5 +6,12 @@ describe('samay.js test suite', function(){
 		expect(+samay().originalDate === +(new Date)).toBe(true)
 	});
 
+	it('constructor with current date object timing', function() {
+		expect(+samay(new Date).originalDate === +(new Date)).toBe(true)
+	});
+
+	it('format test YYYYMMDD against format MM/DD/YYYY HH:mm', function() {
+		expect(samay('20180330', 'YYYYMMDD').format('MM/DD/YYYY HH:mm')).toBe('03/30/2018 00:00')
+	});
 
 });
